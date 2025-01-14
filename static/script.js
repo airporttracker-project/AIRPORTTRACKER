0970105244355096bf66357be5dcecc89d9166a5
Kaan Yazıcıoğlu <register@kaanyazicioglu.com>
2025-01-14 18:20:24 +0100

    frontend update

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.getElementById('flightForm').addEventListener('submit', function(event) {
     const flightInput = document.getElementById('flight');
     if (!flightInput.value.trim()) {
-        alert('Please enter a valid flight ID');
+        alert('Please enter a valid flight ID.');
         event.preventDefault();
     } else {
         alert('Searching for flight: ' + flightInput.value);
