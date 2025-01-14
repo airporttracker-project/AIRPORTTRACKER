47c541bdc71b37a8b55b879e5529bfe23b3c3809
Caner Akcasu <canera5467@gmail.com>
2025-01-14 17:57:34 +0100

    backend done

--- AirportTracker/settings.py
+++ AirportTracker/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,5 +67,4 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- AirportTracker/views.py
+++ AirportTracker/views.py
@@ -1,45 +1,27 @@
 import requests
 from django.shortcuts import render
-from django.http import JsonResponse
-def search_flight(request):
-    flight_query = request.GET.get('flight', '')
-    if flight_query:
-        # Burada flight_query kullanarak bir işlem yapabilirsiniz
-        context = {'flight': flight_query}
-        return render(request, 'search_results.html', context)
-    else:
-        return render(request, 'index.html', {'error': 'No flight provided!'})
-
-
-
 
 def index(request):
+    """Anasayfa için şablonu döndürür."""
     return render(request, 'index.html')
 
 def search_flight(request):
-    flight_id = request.GET.get('flight_id')
+    """Uçuş bilgilerini arar ve sonuçları döndürür."""
+    flight_id = request.GET.get('flight')  # Uçuş ID parametresi
     if not flight_id:
-        return JsonResponse({'error': 'Flight ID is required'}, status=400)
+        return render(request, 'index.html', {'error': 'Flight ID is required!'})
 
+    # Flight API çağrısı
     flight_url = f"https://api.magicapi.dev/api/v1/aedbx/aerodatabox/flights/Number/{flight_id}?withAircraftImage=true&withLocation=false"
-    headers = {'x-magicapi-key': 'cm5ppmxs30001js032fsnet77'} #apikey update
-
+    headers = {'x-magicapi-key': 'cm5ppmxs30001js032fsnet77'}
     flight_response = requests.get(flight_url, headers=headers)
-    if flight_response.status_code == 204:
-        return JsonResponse({'error': 'Flight not found'}, status=404)
+
+    if flight_response.status_code != 200:
+        return render(request, 'index.html', {'error': 'Flight not found!'})
 
     flight_data = flight_response.json()
     if not flight_data:
-        return JsonResponse({'error': 'No data available for this flight'}, status=404)
+        return render(request, 'index.html', {'error': 'No data available for this flight!'})
 
-    airport_icao = flight_data[0]['arrival']['airport']['icao']
-    airport_url = f"https://api.magicapi.dev/api/v1/aedbx/aerodatabox/airports/Icao/{airport_icao}?withRunways=false&withTime=false"
-    airport_response = requests.get(airport_url, headers=headers)
-
-    if airport_response.status_code != 200:
-        return JsonResponse({'error': 'Airport data not available'}, status=404)
-
-    return JsonResponse({
-        'flight_data': flight_data[0],
-        'airport_data': airport_response.json()
-    })
+    # Şablon ile sonuçları döndürme
+    return render(request, 'search_results.html', {'flight_data': flight_data[0]})
